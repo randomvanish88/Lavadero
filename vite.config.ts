9d170639f35a13860f94a091d32e295bdeb0fa1f
agustin gauna <randomvanish88@gmail.com>
2026-04-09 19:07:43 -0300

    fix vite config syntax

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Lavadero/', // 👈 ESTO ES CLAVE
+  base: '/Lavadero/', // 👈 CLAVE
 
   plugins: [
     react(),
